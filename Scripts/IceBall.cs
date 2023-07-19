df399de17adee31d216c779d526617033c248818
gasparo8 <gasparo8@msu.edu>
2023-07-18 20:21:01 -0400

    iceBall Player Damage Implementation LVL2

--- Scripts/IceBall.cs
+++ Scripts/IceBall.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,19 +8,41 @@ public class IceBall : MonoBehaviour
     [HideInInspector]
     public float speed;
     private Rigidbody2D iceBall;
+    private Player player;
+    private int damage = 35;
+
 
     // Start is called before the first frame update
     void Start()
     {
         iceBall = GetComponent<Rigidbody2D>();
-        speed = -2f;
+
+        // Initialize the player variable.
+        if (GameObject.FindWithTag("Player") != null)
+        {
+            player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        }
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        speed = -2f;
+
         //applying speed to x and leaving the y alone by keeping it at iceBall.velocity.y
         iceBall.velocity = new Vector2(speed, iceBall.velocity.y);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.TakeDamage(damage);
+            Debug.Log("Player took DAMAGE");
+            Destroy(gameObject);
+        }
     }
 }
