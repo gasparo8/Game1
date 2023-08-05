8a35219ac0f2bb0478ab01beb49f40c4e851baa4
gasparo8 <gasparo8@msu.edu>
2023-08-05 11:15:29 -0400

    Began Demon LR Animation and StateMachine Script

---
+++ Scripts/Demon_Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demon_Movement : StateMachineBehaviour
+{
+
+    public float speed = 1.5f;
+    Transform demonR;
+    Transform demonL;
+    Rigidbody2D rb;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        demonL = GameObject.FindGameObjectWithTag("Demon Left Point").transform;
+        demonR = GameObject.FindGameObjectWithTag("Demon Right Point").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector2 target = new Vector2(demonR.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+     
+    }
+
+}
