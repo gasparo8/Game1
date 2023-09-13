971dc839dd4291e240fd1eeca9afd73b42a6a8b3
gasparo8 <gasparo8@msu.edu>
2023-09-12 22:15:14 -0400

    Raining Arrows Animation LVL5

--- Scripts/IdleBehavior.cs
+++ Scripts/IdleBehavior.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class IdleBehavior : StateMachineBehaviour
 {
+    public GameObject arrowPrefab;
+    public float spawnInterval = 5f;
+    public Vector2 spawnRange = new Vector2(-10, 10);
+
+    private float timer = 0f;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        timer = 0f;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
         {
-            animator.SetTrigger("Attack");
+            timer = 0f;
+
+            Vector3 spawnPosition = new Vector3(Random.Range(spawnRange.x, spawnRange.y), 10, 0);
+            Instantiate(arrowPrefab, spawnPosition, Quaternion.identity);
         }
     }
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+
+    // OnStateExit is called when a transition ends, and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        // Perform cleanup when the state exits
     }
 }
