bc5692ae17af7a2d6158d9a5fabd91ae6a2b27f6
gasparo8 <gasparo8@msu.edu>
2023-10-03 19:48:13 -0400

    Demon Health Bar Implemented and Working

--- Scripts/Demon.cs
+++ Scripts/Demon.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Demon : MonoBehaviour
 {
-    public int maxHealth = 100;
+    public int maxHealth = 1000;
     public int currentHealth;
     private Rigidbody2D rb;
     public Animator animator;
+  
+    public DemonHealth demonhealthBar;
 
     [SerializeField] private EnemyFlash flashEffect;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +23,20 @@ public class Demon : MonoBehaviour
         animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
+        demonhealthBar.SetMaxHealthDemon(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+      
     }
+
 
     public void TakeDamage(int damage)
      {
         currentHealth -= damage;
+        demonhealthBar.SetHealthDemon(currentHealth);
 
         {
             flashEffect.Flash();
---
+++ Scripts/DemonHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DemonHealth : MonoBehaviour
+{
+    public Slider demonSlider;
+
+
+    public void SetMaxHealthDemon(int health)
+    {
+        demonSlider.maxValue = health;
+        demonSlider.value = health;
+    }
+
+    public void SetHealthDemon(int health)
+    {
+        demonSlider.value = health;
+    }
+}
