ed745ff23372695b2c474c1cff07e031aae95bae
gasparo8 <gasparo8@msu.edu>
2023-10-06 22:24:06 -0400

    Demon Attack Animations/Script Functional

--- Scripts/Demon.cs
+++ Scripts/Demon.cs
@@ -9,11 +9,10 @@ public class Demon : MonoBehaviour
     public int currentHealth;
     private Rigidbody2D rb;
     public Animator animator;
-  
+
     public DemonHealth demonhealthBar;
 
     [SerializeField] private EnemyFlash flashEffect;
-
 
 
     // Start is called before the first frame update
@@ -29,7 +28,41 @@ public class Demon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+        if (currentHealth <= 1000 && currentHealth >= 666)
+        {
+            animator.SetBool("ArrowRain", true);
+            animator.SetBool("Vulnerable", false);
+            animator.SetBool("IceAttack", false); // Ensure others are set to false
+            animator.SetBool("ComboAttack", false);
+            animator.SetBool("Death", false);
+        }
+
+        else if (currentHealth <= 665 && currentHealth >= 334)
+        {
+            animator.SetBool("ArrowRain", false);
+            animator.SetBool("Vulnerable", false);
+            animator.SetBool("IceAttack", true);
+            animator.SetBool("ComboAttack", false);
+            animator.SetBool("Death", false);
+        }
+
+        else if (currentHealth <= 333 && currentHealth >= 1)
+        {
+            animator.SetBool("ArrowRain", false);
+            animator.SetBool("Vulnerable", false);
+            animator.SetBool("IceAttack", false);
+            animator.SetBool("ComboAttack", true);
+            animator.SetBool("Death", false);
+        }
+
+        else if (currentHealth <= 0)
+        {
+            animator.SetBool("ArrowRain", false);
+            animator.SetBool("Vulnerable", false);
+            animator.SetBool("IceAttack", false);
+            animator.SetBool("ComboAttack", false);
+            animator.SetBool("Death", true);
+        }
     }
 
 
@@ -41,6 +74,5 @@ public class Demon : MonoBehaviour
         {
             flashEffect.Flash();
         }
-
     }
 }
