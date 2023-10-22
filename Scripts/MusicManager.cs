03ec4a3a3d214866e639c63abe69908dccfbf66f
gasparo8 <gasparo8@msu.edu>
2023-10-22 16:32:42 -0400

    Game Audio Fully Implemented

--- Scripts/MusicManager.cs
+++ Scripts/MusicManager.cs
@@ -4,17 +4,15 @@ using UnityEngine.SceneManagement;
 public class MusicManager : MonoBehaviour
 {
     private static MusicManager musicManagerInstance;
-
     public AudioSource backgroundMusic;
+
 
     void Start()
     {
         DontDestroyOnLoad(gameObject);
     }
 
-
     public void ChangeBGM(AudioClip music)
-
     {
         backgroundMusic.Stop();
         backgroundMusic.clip = music;
